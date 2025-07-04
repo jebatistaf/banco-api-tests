5131c1b4d5b42206b22fba4dbd78d11b8e8674f9
Jessica <jessica.batista120293@gmail.com>
2025-07-04 20:43:39 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token em string quando usar credenciais válidas', async() => {
+            const resposta = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+            
+            console.log(resposta.body)
+            console.log(resposta.status)    
+
+            expect(resposta.body.token).to.be.a('string');
+            expect(resposta.status).to.equal(200);
+
+        })
+
+    })
+
+})
