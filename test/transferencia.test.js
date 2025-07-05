dfe18be5c3f41085cf6bfdcb128d9cfb10b32ccf
Jessica <jessica.batista120293@gmail.com>
2025-07-05 19:51:52 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao.js')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe ('Transferencias', () => {
     describe('POST /transferencias', () => {
@@ -13,34 +14,29 @@ describe ('Transferencias', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async() => {
+            const bodyTransferencias = { ...postTransferencias }
+            
             // Realizar Transferencia
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authotization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
+                .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(201);
 
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10 reais', async() => {
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 7
+
             // Realizar Transferencia
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authotization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 7,
-                    token: ""
-                })
+                .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(422);
 
