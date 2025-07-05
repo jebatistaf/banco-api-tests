e18f4930854a44d068c8cd77f2fe2cbca9a0f45c
Jessica <jessica.batista120293@gmail.com>
2025-07-05 18:02:00 -0300

    Adicionando testes de transferencia

---
+++ test/transferencia.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe ('Transferencias', () => {
+    describe ('POST /transferencias', () => {
+        it ('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async() => {
+            // Capturar o token
+            const respostaLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+            })
+            
+            const token = respostaLogin.body.token
+
+            // Realizar Transferencia
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authotization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 11,
+                    token: ""
+                })
+
+            expect(resposta.status).to.equal(201);
+
+        })
+
+        it ('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10 reais', async() => {
+            // Capturar o token
+            const respostaLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+            })
+            
+            const token = respostaLogin.body.token
+
+            // Realizar Transferencia
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authotization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 7,
+                    token: ""
+                })
+
+            expect(resposta.status).to.equal(422);
+
+        })
+
+    })
+
+})
